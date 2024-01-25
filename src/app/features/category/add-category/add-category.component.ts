50a68a7cafa2b8c8bd91eb3f962e7c13c2058d4c
Dutu Denis <dutudenis30@yahoo.com>
2024-01-25 17:05:57 +0200

    create service for add category and start service for getAllCategories

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -1,24 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request.model';
+import { CategoryService } from '../services/category.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-add-category',
   templateUrl: './add-category.component.html',
-  styleUrls: ['./add-category.component.scss']
+  styleUrls: ['./add-category.component.scss'],
 })
-export class AddCategoryComponent {
-
+export class AddCategoryComponent implements OnDestroy {
   model: AddCategoryRequest;
 
-  constructor() {
-    this.model = 
-    {
+  private addCategorySubscription?: Subscription;
+
+  constructor(private categoryService: CategoryService) {
+    this.model = {
       name: '',
-      urlHandle: ''
+      urlHandle: '',
     };
   }
-  onFormSubmit() {
-    console.log(this.model);
+  ngOnDestroy(): void {
+    this.addCategorySubscription?.unsubscribe();
   }
 
+  onFormSubmit() {
+    this.addCategorySubscription = this.categoryService
+      .addCategory(this.model)
+      .subscribe({
+        next: (response) => console.log('Category added successfully'),
+        error: (error) => alert('There was an error adding the category \n'),
+        complete: () => console.log('Completed'),
+      });
+  }
 }
---
+++ src/app/features/category/services/category.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { AddCategoryRequest } from '../models/add-category-request.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  constructor(private http: HttpClient) {}
+
+  addCategory(model: AddCategoryRequest): Observable<void> {
+    return this.http.post<void>(`https://localhost:7094/api/Categories`, model);
+  }
+
+  getAllCategories(): Observable<void> {
+    return this.http.get<any>(`https://localhost:7094/api/Categories`);
+  }
+}
