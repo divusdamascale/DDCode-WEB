11f1a9ab70250e40ad6c46764fef5ac943843785
Dutu Denis <dutudenis30@yahoo.com>
2024-01-27 22:02:07 +0200

    bind service with button to add blogpost

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { AddBlogPost } from '../models/add-blog-post.model';
+import { BlogPostService } from '../services/blog-post.service';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
   styleUrls: ['./add-blogpost.component.scss'],
 })
-export class AddBlogpostComponent {
+export class AddBlogpostComponent implements OnDestroy {
   model: AddBlogPost;
+  addBlogPostSubscription?: Subscription;
 
-  constructor() {
+  constructor(
+    private blogPostService: BlogPostService,
+    private router: Router
+  ) {
     this.model = {
       title: '',
       content: '',
@@ -21,7 +28,14 @@ export class AddBlogpostComponent {
       urlHandle: '',
     };
   }
+  ngOnDestroy(): void {
+    this.addBlogPostSubscription?.unsubscribe();
+  }
   onFormSubmit() {
-    console.log(this.model);
+    this.addBlogPostSubscription = this.blogPostService
+      .createBlogPost(this.model)
+      .subscribe((response) => {
+        this.router.navigateByUrl('/admin/blogposts');
+      });
   }
 }
